90e35465766ca8b1666ed5371db86dee6a6fa432
msVill <msv.ltmc@gmail.com>
2026-02-15 21:12:01 -0800

    Added: data to string array

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -1,6 +1,7 @@
 // comsc 210 | Martha Stephanie Villalta | Lab #7
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 
@@ -11,9 +12,19 @@ void displayArray(string *, int);
 int main(){
     int size = 5;
 
+    string *names = new string[size];
     //create a dynamic string array. pop. w/5 names.
+    // the array is going to accept 5 names, so I just needs to hard code 5 names
+    // using pointer notation (we are using pointers, X array notation.)
+    *(names + 0) = "Ricky";
+    *(names + 1) = "Melba";
+    *(names + 2) = "Rita";
+    *(names + 3) = "Carlos";
+    *(names + 4) = "Araceli";
     // use functions to reverse & output.
     //!!! use pointer notation (X array notation).
+
+    delete [] names;
     return 0;
 }
 
