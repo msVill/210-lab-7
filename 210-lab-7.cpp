2a787b1db738fda8a17779a4fb8f93cfa7c02675
msVill <msv.ltmc@gmail.com>
2026-02-15 20:59:53 -0800

    Add prorotypes and comments for progress reminders

---
+++ 210-lab-7.cpp
@@ -0,0 +1,20 @@
+// comsc 210 | Martha Stephanie Villalta | Lab #7
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+// function prototypes:
+string* reverseArray(string *, int);
+void displayArray(string *, int);
+
+int main(){
+    int size = 5;
+
+    //create a dynamic string array. pop. w/5 names.
+    // use functions to reverse & output.
+    //!!! use pointer notation (X array notation).
+    return 0;
+}
+
+//reverseArray() function. ?How can I reverse an array?
